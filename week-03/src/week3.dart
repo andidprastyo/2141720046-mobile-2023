0440f3c4f588c2c87a5abde072d7cf5965bc3d49
andidprastyo <2141720046@student.polinema.ac.id>
2023-09-11 09:23:47 +0700

    feat:Assignment 1, Step 3

--- week-03/src/week3.dart
+++ week-03/src/week3.dart
@@ -1,13 +1,18 @@
 void main() {
-  String test = 'test2';
-  if (test == 'test1') {
-    print('Test1');
-  } else if (test == 'test2') {
-    print('Test2');
-  } else {
-    print('Something Else');
+  // String test = 'test2';
+  // if (test == 'test1') {
+  //   print('Test1');
+  // } else if (test == 'test2') {
+  //   print('Test2');
+  // } else {
+  //   print('Something Else');
+  // }
+
+  // if (test == 'test2') print('Test2 Again');
+
+
+  String test = 'true';
+  if (test == 'true') {
+    print('kebenaran');
   }
-
-  if (test == 'test2') print('Test2 Again');
-
 }
