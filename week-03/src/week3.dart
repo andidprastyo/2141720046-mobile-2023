b57195dc32922a57e2f459875c1177efe1a67f9e
andidprastyo <2141720046@student.polinema.ac.id>
2023-09-11 09:32:32 +0700

    feat:Assignment 2, Step 3

--- week-03/src/week3.dart
+++ week-03/src/week3.dart
@@ -1,4 +1,6 @@
 void main() {
+  // Praktikum 1 Langkah 1 :
+
   // String test = 'test2';
   // if (test == 'test1') {
   //   print('Test1');
@@ -10,9 +12,25 @@ void main() {
 
   // if (test == 'test2') print('Test2 Again');
 
+  // Praktikum 1 Langkah 3 :
 
-  String test = 'true';
-  if (test == 'true') {
-    print('kebenaran');
-  }
+  // String test = 'true';
+  // if (test == 'true') {
+  //   print('kebenaran');
+  // }
+
+  // Praktikum 2 Langkah 1 :
+  int counter = 65;
+  
+  // while (counter < 33) {
+  //   print(counter);
+  //   counter++;
+  // }
+
+  // Praktikum 2 Langkah 3 :
+
+  do {
+    print(counter);
+    counter++;
+  } while (counter < 77);
 }
