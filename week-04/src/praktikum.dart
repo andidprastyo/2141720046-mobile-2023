46267c59779b35e6d24f1b27e418830fca412759
andidprastyo <2141720046@student.polinema.ac.id>
2023-09-18 09:32:04 +0700

    feat:Assignment 2, Set

--- week-04/src/praktikum.dart
+++ week-04/src/praktikum.dart
@@ -1,20 +1,37 @@
 void main() {
+  // // Step 1 - List
+
+  // var list = [1, 2, 3];
+  // assert(list.length == 3);
+  // assert(list[1] == 2);
+  // print(list.length);
+  // print(list[1]);
+
+  // list[1] = 1;
+  // assert(list[1] == 1);
+  // print(list[1]);
+
+  // // Step 3
+  // final List<String?> list_nim = List.filled(6, null);
+  // list_nim[1] = 'Nama: Andi Dwi Prastyo, NIM:2141720046';
+  // list_nim[2] = 'Nama: Bagus Rezky Adhyaksa, NIM:2141720210';
+
+  // print(list_nim);
+
+  // Praktikum 2 - Set
+
   // Step 1
 
-  var list = [1, 2, 3];
-  assert(list.length == 3);
-  assert(list[1] == 2);
-  print(list.length);
-  print(list[1]);
-
-  list[1] = 1;
-  assert(list[1] == 1);
-  print(list[1]);
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  print(halogens);
 
   // Step 3
-  final List<String?> list_nim = List.filled(6, null);
-  list_nim[1] = 'Nama: Andi Dwi Prastyo, NIM:2141720046';
-  list_nim[2] = 'Nama: Bagus Rezky Adhyaksa, NIM:2141720210';
 
-  print(list_nim);
+  var names1 = <String>{};
+  Set<String> names2 = {};
+  var names3 = {}; // This is a map, not a set.
+
+  print(names1);
+  print(names2);
+  print(names3);
 }
