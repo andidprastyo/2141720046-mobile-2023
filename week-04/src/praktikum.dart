4b9c00e1477c25a4710859cd84c27343c7fde307
andidprastyo <2141720046@student.polinema.ac.id>
2023-09-18 09:10:13 +0700

    feat:Assignment 1, List

--- week-04/src/praktikum.dart
+++ week-04/src/praktikum.dart
@@ -1,3 +1,20 @@
-void main(){
-  
+void main() {
+  // Step 1
+
+  var list = [1, 2, 3];
+  assert(list.length == 3);
+  assert(list[1] == 2);
+  print(list.length);
+  print(list[1]);
+
+  list[1] = 1;
+  assert(list[1] == 1);
+  print(list[1]);
+
+  // Step 3
+  final List<String?> list_nim = List.filled(6, null);
+  list_nim[1] = 'Nama: Andi Dwi Prastyo, NIM:2141720046';
+  list_nim[2] = 'Nama: Bagus Rezky Adhyaksa, NIM:2141720210';
+
+  print(list_nim);
 }
