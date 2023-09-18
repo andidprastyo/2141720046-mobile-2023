9e621c725494103b3c26086ff05ac064ea81e9be
andidprastyo <2141720046@student.polinema.ac.id>
2023-09-18 10:16:32 +0700

    feat:Assignment 3, Map

--- week-04/src/praktikum.dart
+++ week-04/src/praktikum.dart
@@ -20,19 +20,51 @@ void main() {
 
   // Praktikum 2 - Set
 
+  // // Step 1
+
+  // var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  // print(halogens);
+
+  // // Step 3
+
+  // var names1 = <String>{};
+  // Set<String> names2 = {};
+
+  // names1.add('Nama: Andi Dwi Prastyo, NIM:2141720046');
+  // names2.addAll(['Nama: Bagus Rezky Adhyaksa, NIM:2141720210']);
+
+  // print(names1);
+  // print(names2);
+
+  // Praktikum 3 - Map
+
   // Step 1
 
-  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
-  print(halogens);
+  var gifts = {
+    // Key:    Value
+    'first': 'partridge',
+    'second': 'turtledoves',
+    'fifth': 1
+  };
+
+  var nobleGasses = {2: 'helium', 10: 'neon', 18: 2};
+
+  print(gifts);
+  print(nobleGasses);
 
   // Step 3
 
-  var names1 = <String>{};
-  Set<String> names2 = {};
+  var mhs1 = Map<String, String>();
+  mhs1['name'] = 'Andi DWi Prastyo';
+  mhs1['nim'] = '2141720046';
 
-  names1.add('Nama: Andi Dwi Prastyo, NIM:2141720046');
-  names2.addAll(['Nama: Bagus Rezky Adhyaksa, NIM:2141720210']);
+  var mhs2 = Map<int, String>();
+  mhs2[1] = 'Bagus Rezky Adhyaksa';
+  mhs2[2] = '2141720210';
 
-  print(names1);
-  print(names2);
+  mhs1.addAll({'fifth': 'golden rings'});
+  mhs2.addAll({18: 'argon'});
+
+  print(mhs1);
+  print(mhs2);
 }
