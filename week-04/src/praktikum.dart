d4047ea340522d978e4612a8c6d9369097e9e7ce
andidprastyo <2141720046@student.polinema.ac.id>
2023-09-18 10:56:06 +0700

    feat:Assignmnet 4, Operators

--- week-04/src/praktikum.dart
+++ week-04/src/praktikum.dart
@@ -38,33 +38,64 @@ void main() {
 
   // Praktikum 3 - Map
 
+  // // Step 1
+
+  // var gifts = {
+  //   // Key:    Value
+  //   'first': 'partridge',
+  //   'second': 'turtledoves',
+  //   'fifth': 1
+  // };
+
+  // var nobleGasses = {2: 'helium', 10: 'neon', 18: 2};
+
+  // print(gifts);
+  // print(nobleGasses);
+
+  // // Step 3
+
+  // var mhs1 = Map<String, String>();
+  // mhs1['name'] = 'Andi DWi Prastyo';
+  // mhs1['nim'] = '2141720046';
+
+  // var mhs2 = Map<int, String>();
+  // mhs2[1] = 'Bagus Rezky Adhyaksa';
+  // mhs2[2] = '2141720210';
+
+  // mhs1.addAll({'fifth': 'golden rings'});
+  // mhs2.addAll({18: 'argon'});
+
+  // print(mhs1);
+  // print(mhs2);
+
+  // Praktikum 4 - Spread & Control Flow Operators
+
   // Step 1
-
-  var gifts = {
-    // Key:    Value
-    'first': 'partridge',
-    'second': 'turtledoves',
-    'fifth': 1
-  };
-
-  var nobleGasses = {2: 'helium', 10: 'neon', 18: 2};
-
-  print(gifts);
-  print(nobleGasses);
+  var list = [1, 2, 3];
+  var list2 = [0, ...list];
+  print(list);
+  print(list2);
+  print(list2.length);
 
   // Step 3
+  var list1 = [1, 2, null];
+  print(list1);
+  var list3 = [2141720046 , ...list1];
+  print(list3);
 
-  var mhs1 = Map<String, String>();
-  mhs1['name'] = 'Andi DWi Prastyo';
-  mhs1['nim'] = '2141720046';
+  // Step 4
+  var nav = ['Home', 'Furniture', 'Plants', if (false) 'Outlet'];
+  print(nav);
 
-  var mhs2 = Map<int, String>();
-  mhs2[1] = 'Bagus Rezky Adhyaksa';
-  mhs2[2] = '2141720210';
+  // Step 5
+  var login = 'manager';
+  var nav2 = ['Home', 'Furniture', 'Plants', if (login case 'manager') 'Inventory'];
+  print('nav2: $nav2');
 
-  mhs1.addAll({'fifth': 'golden rings'});
-  mhs2.addAll({18: 'argon'});
+  // Step 6
 
-  print(mhs1);
-  print(mhs2);
+  var listOfInts = [1, 2, 3];
+  var listOfStrings = ['#0', for (var i in listOfInts) '#$i'];
+  assert (listOfStrings[1] == '#1');
+  print(listOfStrings);
 }
