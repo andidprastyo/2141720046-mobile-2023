0f5c341cc47568e842000c7b05bd3e655ac9f8ab
andidprastyo <2141720046@student.polinema.ac.id>
2023-10-01 19:35:48 +0700

    feat:Minor Update

--- week-05/hello_world/lib/main.dart
+++ week-05/hello_world/lib/main.dart
@@ -1,58 +1,90 @@
+import 'package:flutter/material.dart';
+import 'basic_widgets/image_widget.dart';
+import 'basic_widgets/datetime_pickers.dart';
 import 'basic_widgets/dialog_widget.dart';
+import 'basic_widgets/input_selection_widget.dart';
+import 'basic_widgets/scaffold_widget.dart';
+
+void main(){
+  runApp(const TugasPraktikum());
+}
+
+class TugasPraktikum extends StatelessWidget{
+  const TugasPraktikum({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      home: Row(
+        children: [
+          // Flexible(child: MyScaffoldWidget()),
+          // Flexible(child: MyInputSelectionWidget()),
+          // Flexible(child: MyDialogWidget()),
+          Flexible(child: MyDateTimePickers()),
+        ],
+      ),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: MyLayout(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyLayout extends StatelessWidget {
-  const MyLayout({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        child: const Text('Show alert'),
-        onPressed: () {
-          showAlertDialog(context);
-        },
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const MyImageWidget(),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
-}
-
-showAlertDialog(BuildContext context) {
-  // set up the button
-  Widget okButton = TextButton(
-    child: const Text("OK"),
-    onPressed: () {
-      Navigator.pop(context);
-    },
-  );
-
-  // set up the AlertDialog
-  AlertDialog alert = AlertDialog(
-    title: const Text("My title"),
-    content: const Text("This is my message."),
-    actions: [
-      okButton,
-    ],
-  );
-
-  // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
 }
