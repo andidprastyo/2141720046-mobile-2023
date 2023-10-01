679d556e463aea5bf2d61ae656d1d83bfe0f8dc3
andidprastyo <2141720046@student.polinema.ac.id>
2023-10-01 19:10:46 +0700

    feat:Added Widgets for Praktikum 4

--- week-05/hello_world/lib/basic_widgets/dialog_widget.dart
+++ week-05/hello_world/lib/basic_widgets/dialog_widget.dart
@@ -1,7 +1,7 @@
-import 'basic_widgets/dialog_widget.dart';
+import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyDialogWidget extends StatelessWidget {
+  const MyDialogWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
