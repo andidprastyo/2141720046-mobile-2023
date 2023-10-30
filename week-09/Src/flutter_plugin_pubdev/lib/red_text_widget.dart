be78612e8222068b8f2f243d74c331955a55570f
Andi Dwi Prastyo <2141720046@student.polinema.ac.id>
2023-10-30 11:33:42 +0700

    feat:Added Step 5

--- week-09/Src/flutter_plugin_pubdev/lib/red_text_widget.dart
+++ week-09/Src/flutter_plugin_pubdev/lib/red_text_widget.dart
@@ -2,7 +2,10 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class RedTextWidget extends StatelessWidget {
-  const RedTextWidget({Key? key}) : super(key: key);
+
+  final String text;
+
+  const RedTextWidget({Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
