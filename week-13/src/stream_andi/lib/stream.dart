a8055b7199ecec237067bdafb8e1b16d17d0ebc9
Andi Dwi Prastyo <2141720046@student.polinema.ac.id>
2023-11-20 10:28:50 +0700

    W13: Jawaban Soal 2

---
+++ week-13/src/stream_andi/lib/stream.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorSteam {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.black,
+    Colors.red,
+    Colors.green,
+    Colors.pink,
+    Colors.purple,
+  ];
+}
